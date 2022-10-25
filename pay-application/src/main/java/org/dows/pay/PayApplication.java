185b5e75ca89c71f854df8c9b315e12024d72f72
geeker.lait <lait.zhang@gmail.com>
2022-10-25 21:32:50 +0800

    fix

--- pay-application/src/main/java/org/dows/pay/PayApplication.java
+++ pay-application/src/main/java/org/dows/pay/PayApplication.java
@@ -3,7 +3,7 @@ package org.dows.pay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.dows.pay.**"})
+@SpringBootApplication(scanBasePackages = {"org.dows.*"})
 public class PayApplication {
 
     public static void main(String[] args) {
