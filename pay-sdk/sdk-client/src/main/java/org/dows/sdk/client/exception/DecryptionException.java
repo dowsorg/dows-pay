c5ea725b8783b7c1904fdc6e47d3b1962573670c
lait.zhang <zhang_yingliang@163.com>
2023-06-04 17:35:49 +0800

    remove         <module>pay-spider</module>                <module>pay-sdk</module>

--- pay-sdk/sdk-client/src/main/java/org/dows/sdk/client/exception/DecryptionException.java
+++
@@ -1,9 +0,0 @@
-package org.dows.sdk.client.exception;
-
-public class DecryptionException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DecryptionException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-}
--- pay-spider/src/main/resources/ftl/zijieapi.ftl
+++
@@ -1,8 +0,0 @@
-dows:
-  sdk:
-    zijie:
-<#list beanSchemas as bs>
-    <#list methods as method>
-      "${bs.pkg!""}${bs.name?cap_first!""}.${method.name?uncap_first!""}": ${method.url!""}
-    </#list>
-</#list>
