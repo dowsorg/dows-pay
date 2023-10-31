bc02bbcd1f81831cc2f99ba1e6c06f79d3a01a2f
zengcb <861464602@qq.com>
2023-10-31 10:25:23 +0800

    commit

--- pay-rest/pay-user-rest/src/main/java/org/dows/pay/rest/user/v1/OrderPayRest.java
+++ pay-rest/pay-user-rest/src/main/java/org/dows/pay/rest/user/v1/OrderPayRest.java
@@ -1,6 +1,7 @@
 package org.dows.pay.rest.user.v1;
 
 
+import com.github.binarywang.wxpay.bean.ecommerce.TransactionsResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.dows.framework.api.Response;
 import org.dows.pay.api.PayResponse;
 import org.dows.pay.biz.OrderPayBiz;
 import org.dows.pay.form.PayTransactionForm;
+import org.dows.pay.weixin.WeixinPayHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ public class OrderPayRest {
 
     private final OrderPayBiz orderPayBiz;
 
+    private final WeixinPayHandler weixinPayHandler;
+
     @PostMapping("/order/topay")
     @ApiOperation(value = "用户支付")
     public Response<PayResponse> toPay(@RequestBody PayTransactionForm payTransactionForm) {
@@ -31,5 +35,11 @@ public class OrderPayRest {
         return Response.ok();
     }
 
+    @PostMapping("/orderpay/topayNoAcc")
+    @ApiOperation(value = "去支付无分账")
+    public Response topayNoAcc(@RequestBody PayTransactionForm payTransactionForm) {
+        TransactionsResult.JsapiResult result= weixinPayHandler.toPayNoAcc(payTransactionForm);
+        return Response.ok(result);
+    }
 
 }
